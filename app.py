8f15112dc073f2fc41560ea12e9c642a0ab154b2
tejasdhatrika <tejasdhatrika42@gmail.com>
2024-07-18 16:14:49 -0400

    Updating app.py

--- app.py
+++ app.py
@@ -31,4 +31,6 @@ def predict():
     return jsonify({'sentiment': sentiment})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=5000)
+    #app.run(host='0.0.0.0', port=5000)
